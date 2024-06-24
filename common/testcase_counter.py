58ea2a7deb6cc494c870430e8b6cc7c19700593e
郑秋杰 <15881086121@163.com>
2024-06-24 15:13:37 +0800

    添加log日志message

--- common/testcase_counter.py
+++ common/testcase_counter.py
@@ -1,3 +1,5 @@
+from common.log_set import log
+
 counter = 0
 
 
@@ -7,7 +9,8 @@ def count(data):
     separator = '>' * 20
     counter += 1  # 递增类级别的测试用例计数器
     yellow_text = f"\033[33m\033[1m{separator}执行第{counter}条用例{separator}\033[0m"
-    print(f"\n{yellow_text}\n")
+    print_data = f"\n{yellow_text}\n"
+    log.info(print_data)
     return data
 
 
