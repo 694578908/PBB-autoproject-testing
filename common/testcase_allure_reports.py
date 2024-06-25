d50b600ddce0072895a7c8eac0fb69db8bb7573a
郑秋杰 <15881086121@163.com>
2024-06-25 16:08:49 +0800

    修改接口响应的message

--- common/testcase_allure_reports.py
+++ common/testcase_allure_reports.py
@@ -107,9 +107,8 @@ def allure_error_message(key, validate_value, error_messages, actual_value):
 # request_util.py:接口响应消息
 def allure_requesttime_success(status_code):
     log_status = f'接口响应成功code:{status_code}'
-    code_message = '预期接口响应code:200'
-    allure.attach(code_message, name=log_status)
-    log.info(f"{code_message},{log_status} ")
+    allure.attach(name=log_status)
+    log.info(f"{log_status} ")
 
 
 def allure_requesttime_fail(status_code):
