d83ea7e60de48513d2c6307e712a52a7f7d0216e
郑秋杰 <15881086121@163.com>
2024-07-03 18:53:47 +0800

    完善yaml的判断

--- testcases/test_parametrize.py
+++ testcases/test_parametrize.py
@@ -17,21 +17,21 @@ class TestClient:
         if key in res:
             read_redis(read_config_redis_data)
 
-#     @allure.feature('C端')
-#     @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'C_module'))
-#     def test_case_C_module(self, case):
-#         case_request(case)
-#
-#     @allure.feature('B端')
-#     @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'B_module'))
-#     def test_case_B_module(self, case):
-#         case_request(case)
-#
-#
-# @allure.epic('聘播播-后台管理自动化测试')
-# class TestBackground:
-#     @allure.feature('后台管理')
-#     @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'PBB_backstage_login'))
-#     def test_case_backstage_login(self, case):
-#         case_request(case)
-#         # login_Image_recognition()
+    @allure.feature('C端')
+    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'C_module'))
+    def test_case_C_module(self, case):
+        case_request(case)
+
+    @allure.feature('B端')
+    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'B_module'))
+    def test_case_B_module(self, case):
+        case_request(case)
+
+
+@allure.epic('聘播播-后台管理自动化测试')
+class TestBackground:
+    @allure.feature('后台管理')
+    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'PBB_backstage_login'))
+    def test_case_backstage_login(self, case):
+        case_request(case)
+        # login_Image_recognition()
