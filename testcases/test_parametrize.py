bb2c55ad875198104dcdc72a1b36dfae49e41a23
郑秋杰 <15881086121@163.com>
2024-07-18 19:20:00 +0800

    修改用例

--- testcases/test_parametrize.py
+++ testcases/test_parametrize.py
@@ -17,15 +17,15 @@ class TestClient:
         if key in res:
             read_redis(read_config_redis_data)
 
-#     @allure.feature('C端')
-#     @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'C_module'))
-#     def test_case_C_module(self, case):
-#         case_request(case)
-#
-#     @allure.feature('B端')
-#     @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'B_module'))
-#     def test_case_B_module(self, case):
-#         case_request(case)
+    @allure.feature('C端')
+    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'C_module'))
+    def test_case_C_module(self, case):
+        case_request(case)
+
+    @allure.feature('B端')
+    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'B_module'))
+    def test_case_B_module(self, case):
+        case_request(case)
 
 
 @allure.epic('聘播播-后台管理自动化测试')
