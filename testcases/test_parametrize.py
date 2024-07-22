93e5d796b7377daf86e981171629f878e90f8bf2
郑秋杰 <15881086121@163.com>
2024-07-22 13:56:53 +0800

    拆分测试用例模块

--- testcases/test_parametrize.py
+++ testcases/test_parametrize.py
@@ -10,7 +10,7 @@ from common.testcase_request_response import case_request
 @allure.epic('聘播播-客户端自动化测试')
 class TestClient:
     @allure.feature('客户端-用户登陆')
-    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'PBB_login'))
+    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('PBB_app_login.yml', 'PBB_app_login'))
     def test_case_login(self, case, read_config_redis_data):
         res = case_request(case)
         host, password, port, db, key = read_config_redis_data
@@ -18,12 +18,12 @@ class TestClient:
             read_redis(read_config_redis_data)
 
     @allure.feature('C端')
-    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'C_module'))
+    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('C_module.yml', 'C_module'))
     def test_case_C_module(self, case):
         case_request(case)
 
     @allure.feature('B端')
-    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'B_module'))
+    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('B_module.yml', 'B_module'))
     def test_case_B_module(self, case):
         case_request(case)
 
@@ -31,6 +31,6 @@ class TestClient:
 @allure.epic('聘播播-后台管理自动化测试')
 class TestBackground:
     @allure.feature('后台管理')
-    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('test_case.yml', 'PBB_backstage_login'))
+    @pytest.mark.parametrize('case', YamlUtil().read_testcase_yaml('PBB_backstage_login.yml', 'PBB_backstage_login'))
     def test_case_backstage_login(self, case):
         case_request(case)
